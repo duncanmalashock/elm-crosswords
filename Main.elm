f8cfd8f55ce47466d27272236575c08bd8af3553
Duncan Malashock <duncanmalashock@gmail.com>
2018-01-15 15:46:24 -0500

    Model Squares with coordinates instead of id relationships

--- Main.elm
+++ Main.elm
@@ -1,6 +1,8 @@
 module Main exposing (main)
 
 import Html exposing (Html, div, text)
+import Html.Attributes exposing (class, style)
+import List.Extra
 
 
 main : Program Never Model Msg
@@ -13,127 +15,66 @@ main =
         }
 
 
-type alias Puzzle =
-    { grid : Grid
-    }
-
-
 type alias Grid =
     List Square
-
-
-type Selected
-    = Selected
-    | NotSelected
 
 
 type alias Letter =
     Char
 
 
-type alias Id =
-    Int
-
-
 type Square
-    = Open Id Selected Letter Neighbors
-    | Filled Id Neighbors
-
-
-type NeighborDirection
-    = Left
-    | Right
-    | Up
-    | Down
+    = Open Coordinates Letter
+    | Filled Coordinates
 
 
 initGrid : Grid
 initGrid =
-    [ Open 0 NotSelected 'a' { up = Nothing, down = Just 3, right = Just 1, left = Nothing }
-    , Open 1 NotSelected 'a' { up = Nothing, down = Just 4, right = Just 2, left = Just 0 }
-    , Open 2 NotSelected 'a' { up = Nothing, down = Just 5, right = Nothing, left = Just 1 }
-    , Open 3 NotSelected 'a' { up = Just 0, down = Just 6, right = Just 4, left = Nothing }
-    , Filled 4 { up = Just 1, down = Just 7, right = Just 5, left = Just 3 }
-    , Open 5 NotSelected 'a' { up = Just 2, down = Just 8, right = Nothing, left = Just 4 }
-    , Open 6 NotSelected 'a' { up = Just 3, down = Nothing, right = Just 7, left = Nothing }
-    , Open 7 NotSelected 'a' { up = Just 4, down = Nothing, right = Just 8, left = Just 6 }
-    , Open 8 NotSelected 'a' { up = Just 5, down = Nothing, right = Nothing, left = Just 7 }
+    [ Open { x = 0, y = 0 } 'A'
+    , Open { x = 1, y = 0 } 'B'
+    , Open { x = 2, y = 0 } 'C'
+    , Open { x = 0, y = 1 } 'D'
+    , Filled { x = 1, y = 1 }
+    , Open { x = 2, y = 1 } 'E'
+    , Open { x = 0, y = 2 } 'F'
+    , Open { x = 1, y = 2 } 'G'
+    , Open { x = 2, y = 2 } 'H'
     ]
 
 
-getNeigbor : Grid -> Square -> (Neighbors -> Maybe Id) -> Maybe Square
-getNeigbor grid square accessor =
-    square
-        |> neighbors
-        |> accessor
-        |> Maybe.andThen (getSquareById grid)
-
-
-upNeighbor : Grid -> Square -> Maybe Square
-upNeighbor grid square =
-    getNeigbor grid square .up
-
-
-downNeighbor : Grid -> Square -> Maybe Square
-downNeighbor grid square =
-    getNeigbor grid square .down
-
-
-leftNeighbor : Grid -> Square -> Maybe Square
-leftNeighbor grid square =
-    getNeigbor grid square .left
-
-
-rightNeighbor : Grid -> Square -> Maybe Square
-rightNeighbor grid square =
-    getNeigbor grid square .right
-
-
-getSquareById : Grid -> Id -> Maybe Square
-getSquareById grid id =
+getSquareByCoordinates : Grid -> Coordinates -> Maybe Square
+getSquareByCoordinates grid coordinates =
     List.filter
         (\square ->
-            idIs id (squareId square)
+            coordinatesEqual (squareCoordinates square) coordinates
         )
         grid
         |> List.head
 
 
-idIs : Id -> Id -> Bool
-idIs id1 id2 =
-    id1 == id2
+coordinatesEqual : Coordinates -> Coordinates -> Bool
+coordinatesEqual coords1 coords2 =
+    coords1 == coords2
 
 
-squareId : Square -> Id
-squareId square =
+squareCoordinates : Square -> Coordinates
+squareCoordinates square =
     case square of
-        Open id _ _ _ ->
-            id
+        Open coords _ ->
+            coords
 
-        Filled id _ ->
-            id
-
-
-neighbors : Square -> Neighbors
-neighbors square =
-    case square of
-        Open _ _ _ neighbors ->
-            neighbors
-
-        Filled _ neighbors ->
-            neighbors
+        Filled coords ->
+            coords
 
 
-type alias Neighbors =
-    { up : Maybe Id
-    , down : Maybe Id
-    , right : Maybe Id
-    , left : Maybe Id
+type alias Coordinates =
+    { x : Int
+    , y : Int
     }
 
 
 type alias Model =
-    { puzzle : Puzzle
+    { grid : Grid
     }
 
 
@@ -148,7 +89,7 @@ subscriptions model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( { puzzle = { grid = initGrid } }, Cmd.none )
+    ( { grid = initGrid }, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -156,38 +97,66 @@ update msg model =
     ( model, Cmd.none )
 
 
+
+-- placeholder view code for now
+
+
 squareView : Square -> Html Msg
 squareView square =
+    case square of
+        Open coords letter ->
+            div
+                [ class "square--open"
+                , style
+                    [ ( "width", "32px" )
+                    , ( "height", "32px" )
+                    , ( "display", "inline-block" )
+                    , ( "box-sizing", "border-box" )
+                    , ( "vertical-align", "top" )
+                    , ( "padding", "8px 0" )
+                    , ( "text-align", "center" )
+                    , ( "border-right", "1px solid gray" )
+                    , ( "border-bottom", "1px solid gray" )
+                    ]
+                ]
+                [ text <| String.fromChar letter ]
+
+        Filled coords ->
+            div
+                [ class "square--filled"
+                , style
+                    [ ( "width", "32px" )
+                    , ( "height", "32px" )
+                    , ( "display", "inline-block" )
+                    , ( "background-color", "black" )
+                    , ( "box-sizing", "border-box" )
+                    , ( "vertical-align", "top" )
+                    ]
+                ]
+                []
+
+
+gridToRows : Grid -> List (List Square)
+gridToRows grid =
     let
-        squareContent square =
-            case square of
-                Open id _ letter _ ->
-                    "[" ++ toString letter ++ "]"
-
-                Filled id _ ->
-                    "[#]"
+        hasSameY square1 square2 =
+            (squareCoordinates square1).y == (squareCoordinates square2).y
     in
-        div []
-            [ text <| squareContent square ]
-
-
-newPlacement : ( Int, Int ) -> NeighborDirection -> ( Int, Int )
-newPlacement ( x, y ) dir =
-    case dir of
-        Up ->
-            ( x, y - 10 )
-
-        Down ->
-            ( x, y + 10 )
-
-        Left ->
-            ( x - 10, y )
-
-        Right ->
-            ( x + 10, y )
+        grid
+            |> List.sortBy (squareCoordinates >> .y)
+            |> List.Extra.groupWhile hasSameY
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        (List.map squareView model.puzzle.grid)
+    div
+        [ style
+            [ ( "display", "inline-block" )
+            , ( "border-left", "1px solid gray" )
+            , ( "border-top", "1px solid gray" )
+            ]
+        ]
+    <|
+        List.map
+            (List.map squareView >> div [])
+            (gridToRows model.grid)
