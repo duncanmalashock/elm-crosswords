493e75467a958ae0fc1bb734c90fda7d496ccb2e
Duncan Malashock <duncanmalashock@gmail.com>
2018-03-10 14:44:05 -0500

    Tests for Grid

--- tests/GridTests.elm
+++ tests/GridTests.elm
@@ -9,18 +9,226 @@ flattenResult =
     Result.map (Grid.flatten)
 
 
+testFlatten : Test
+testFlatten =
+    describe "Grid.flatten"
+        [ test "four-squared Grid" <|
+            \_ ->
+                let
+                    expectedOutput =
+                        Ok
+                            [ LetterSquare ' '
+                            , BlockSquare
+                            , BlockSquare
+                            , LetterSquare ' '
+                            ]
 
--- TODO: tests for:
--- Grid.flatten
--- Grid.empty
--- Grid.blank
--- Grid.toRows
--- Grid.isAcrossEntryStart
--- Grid.isDownEntryStart
--- Grid.squareAtCoordinate
--- Grid.setAtCoordinate
--- Grid.squareAtRight
--- Grid.squareBelow
+                    input =
+                        Grid.fromString 2 2 ".**."
+                            |> Result.map (Grid.flatten)
+                in
+                    Expect.equal input expectedOutput
+        ]
+
+
+testEmpty : Test
+testEmpty =
+    describe "Grid.empty"
+        [ test "is empty with 0 height and width" <|
+            \_ ->
+                let
+                    expectedOutput =
+                        ( [], 0, 0 )
+
+                    emptyGrid =
+                        Grid.empty
+
+                    input =
+                        ( Grid.flatten emptyGrid
+                        , Grid.width emptyGrid
+                        , Grid.height emptyGrid
+                        )
+                in
+                    Expect.equal input expectedOutput
+        ]
+
+
+testBlank : Test
+testBlank =
+    describe "Grid.blank"
+        [ test "is filled with blank letterSquares" <|
+            \_ ->
+                let
+                    expectedOutput =
+                        ( [ LetterSquare (' ')
+                          , LetterSquare (' ')
+                          , LetterSquare (' ')
+                          , LetterSquare (' ')
+                          ]
+                        , 2
+                        , 2
+                        )
+
+                    blankGrid =
+                        Grid.blank 2 2
+
+                    input =
+                        ( Grid.flatten blankGrid
+                        , Grid.width blankGrid
+                        , Grid.height blankGrid
+                        )
+                in
+                    Expect.equal input expectedOutput
+        ]
+
+
+testToRows : Test
+testToRows =
+    describe "Grid.toRows"
+        [ test "returns a nested list of rows with coordinates" <|
+            \_ ->
+                let
+                    blankGrid =
+                        Grid.blank 2 2
+
+                    expectedOutput =
+                        [ [ ( ( 0, 0 ), LetterSquare (' ') )
+                          , ( ( 1, 0 ), LetterSquare (' ') )
+                          ]
+                        , [ ( ( 0, 1 ), LetterSquare (' ') )
+                          , ( ( 1, 1 ), LetterSquare (' ') )
+                          ]
+                        , []
+                        ]
+                in
+                    Expect.equal (Grid.toRows blankGrid) expectedOutput
+        ]
+
+
+testIsAcrossEntry : Test
+testIsAcrossEntry =
+    describe "Grid.isAcrossEntryStart"
+        [ test "returns true if a coordinate begins an across entry" <|
+            \_ ->
+                let
+                    expectedOutput =
+                        Ok True
+
+                    input =
+                        Grid.fromString 2 2 ".**."
+                            |> Result.map
+                                (\g -> Grid.isAcrossEntryStart g ( 0, 0 ))
+                in
+                    Expect.equal input expectedOutput
+        , test "returns false if a coordinate does not begin an across entry" <|
+            \_ ->
+                let
+                    expectedOutput =
+                        Ok False
+
+                    input =
+                        Grid.fromString 2 2 "...."
+                            |> Result.map
+                                (\g -> Grid.isAcrossEntryStart g ( 1, 0 ))
+                in
+                    Expect.equal input expectedOutput
+        , test "returns false if a coordinate is not a letter square" <|
+            \_ ->
+                let
+                    expectedOutput =
+                        Ok True
+
+                    input =
+                        Grid.fromString 2 2 "****"
+                            |> Result.map
+                                (\g -> Grid.isAcrossEntryStart g ( 0, 0 ))
+                in
+                    Expect.equal input expectedOutput
+        ]
+
+
+testIsDownEntry : Test
+testIsDownEntry =
+    describe "Grid.isDownEntryStart"
+        [ test "returns true if a coordinate begins a down entry" <|
+            \_ ->
+                let
+                    expectedOutput =
+                        Ok True
+
+                    input =
+                        Grid.fromString 2 2 ".**."
+                            |> Result.map
+                                (\g -> Grid.isDownEntryStart g ( 0, 0 ))
+                in
+                    Expect.equal input expectedOutput
+        , test "returns false if a coordinate does not begin a down entry" <|
+            \_ ->
+                let
+                    expectedOutput =
+                        Ok False
+
+                    input =
+                        Grid.fromString 2 2 "...."
+                            |> Result.map
+                                (\g -> Grid.isDownEntryStart g ( 0, 1 ))
+                in
+                    Expect.equal input expectedOutput
+        , test "returns false if a coordinate is not a letter square" <|
+            \_ ->
+                let
+                    expectedOutput =
+                        Ok True
+
+                    input =
+                        Grid.fromString 2 2 "****"
+                            |> Result.map
+                                (\g -> Grid.isDownEntryStart g ( 0, 0 ))
+                in
+                    Expect.equal input expectedOutput
+        ]
+
+
+testSquareAtCoordinate : Test
+testSquareAtCoordinate =
+    describe "Grid.squareAtCoordinate"
+        [ test "returns Nothing if out of bounds" <|
+            \_ ->
+                let
+                    expectedOutput =
+                        Ok Nothing
+
+                    input =
+                        Grid.fromString 2 2 "ABCD"
+                            |> Result.map
+                                (\g -> Grid.squareAtCoordinate g ( 3, 3 ))
+                in
+                    Expect.equal input expectedOutput
+        , test "returns a letter square at matching coordinate" <|
+            \_ ->
+                let
+                    expectedOutput =
+                        Ok (Just <| LetterSquare 'B')
+
+                    input =
+                        Grid.fromString 2 2 "ABCD"
+                            |> Result.map
+                                (\g -> Grid.squareAtCoordinate g ( 1, 0 ))
+                in
+                    Expect.equal input expectedOutput
+        , test "returns a block square at matching coordinate" <|
+            \_ ->
+                let
+                    expectedOutput =
+                        Ok (Just <| BlockSquare)
+
+                    input =
+                        Grid.fromString 2 2 "ABC*"
+                            |> Result.map
+                                (\g -> Grid.squareAtCoordinate g ( 1, 1 ))
+                in
+                    Expect.equal input expectedOutput
+        ]
 
 
 testWidth : Test
